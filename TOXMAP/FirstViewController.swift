0d53caecbde8bbe2efd0e29f74ea328c48cbf82a
ss42 <sanjay.shrestha1988@gmail.com>
2016-09-17 12:02:46 -0700

    initial commit

---
+++ TOXMAP/FirstViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FirstViewController.swift
+//  TOXMAP
+//
+//  Created by Sanjay Shrestha on 9/17/16.
+//  Copyright © 2016 NIH. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var mapView: GMSMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        begin()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func begin(){
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+    }
+
+}
+
